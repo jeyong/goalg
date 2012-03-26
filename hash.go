f951b495a671802f81d5862fce57efe33fa8f557
sjy8282 <sjy8282@hotmail.com>
2012-03-27 07:00:18 +0900

    add printAllTable

--- hash.go
+++ hash.go
@@ -12,12 +12,21 @@ func main(){
 	insertItem(13)
 	printTable()
 	fmt.Println("key : ", 13, searchTable(13))
+	fmt.Println("key : ", 13, "isDelete?",  deleteItem(13))
+	fmt.Println("key : ", 13, searchTable(13))
+	fmt.Println("Print all Table")
+	printAllTable()
 }
 func printTable(){
 	for i:=0; i<10; i++{
 		if table[i] != 0{
 			fmt.Println("index : ", i, "value : ", table[i])
 		}
+	}
+}
+func printAllTable(){
+	for i:=0; i<10; i++{
+		fmt.Println(i, " : ", table[i])
 	}
 }
 
@@ -61,5 +70,23 @@ func searchTable(key int) int{
 		return location
 	}
 	return -1
+}
+
+func deleteItem(key int) bool{
+	location := key % 10
+	count := 0
+	for ; table[location] != 0 && table[location] != key && count <10 ; {
+		count++
+		if location == 9{
+			location = 0
+		}else{
+			location++
+		}
+	}
+	if table[location] == key{
+		table[location] = 0
+		return true
+	}
+	return false
 }
 
